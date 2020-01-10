9fc8a7a360d2d897b48a85d36a97ca90613f69ab
leoberteck <stabileleonardo@hotmail.com>
2020-01-10 18:05:35 -0300

    Update icon_mapping.js

--- icon_mapping.js
+++ icon_mapping.js
@@ -2,3 +2,5 @@ const SolinftecIconMapping = {
   alarm: {x: 0, y: 0, width: 32, height: 32, mask: true},
   alert: {x: 32, y: 0, width: 32, height: 32, mask: true}
 }
+
+export default SolinftecIconMapping
